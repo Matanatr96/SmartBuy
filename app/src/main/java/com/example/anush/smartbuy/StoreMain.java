b5707e36c85baa414cbab203608b7213196c4bf4
Anush Mattapalli <anush0620@yahoo.com>
2016-07-05 10:54:52 -0400

    new menu functionality

--- app/src/main/java/com/example/anush/smartbuy/StoreMain.java
+++ app/src/main/java/com/example/anush/smartbuy/StoreMain.java
@@ -6,7 +6,9 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toast;
 
 public class StoreMain extends AppCompatActivity {
 
@@ -22,6 +24,17 @@ public class StoreMain extends AppCompatActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_store_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.orders:
+                Toast.makeText(this, "You have selected the Orders Menu", Toast.LENGTH_SHORT).show();
+                return true;
+        }
+
         return true;
     }
 
